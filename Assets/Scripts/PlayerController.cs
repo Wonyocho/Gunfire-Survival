512b1c9ef1f62f43c3682257ddeb52ab99d8f5a2
wonyocho <dnjsdud767@gmail.com>
2025-08-09 20:20:00 +0900

    feat: 플레이어 컨트롤러 스크립트 추가

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 8f;
+
+    private Rigidbody2D rb;
+    private Vector2 input;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        // Rigidbody 설정 보정
+        rb.gravityScale = 0f;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    void Update()
+    {
+        // 신/구 Input System 상관없이 동작
+        float x = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0);
+        float y = (Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0);
+
+        input = new Vector2(x, y).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = input * moveSpeed;
+    }
+}
